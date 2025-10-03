38f434d88616bc9149d602f96be54e23d09e10b0
chengting0906os <chengting0906os@gmail.com>
2025-10-03 17:42:11 +0800

    refactor: ruff lint

--- src/platform/logging/loguru_io.pyi
+++ src/platform/logging/loguru_io.pyi
@@ -4,29 +4,23 @@ from typing import ParamSpec
 
 from src.platform.logging.loguru_io_config import GeneratorMethod
 
-_P = ParamSpec("_P")
-_T = TypeVar("_T")
-
+_P = ParamSpec('_P')
+_T = TypeVar('_T')
 
 class LoguruIO:
     def __init__(self, custom_logger: Any, reraise: bool = ...) -> None: ...
-
     def log_args_kwargs_content(
         self, *args: Any, yield_method: Optional[GeneratorMethod] = ..., **kwargs: Any
     ) -> None: ...
-
     def log_return_content(
         self, return_value: Any, yield_method: Optional[GeneratorMethod] = ...
     ) -> None: ...
-
     def mask_sensitive(self, data: Any) -> Any: ...
-
     def __call__(self, func: Callable[_P, _T]) -> Callable[_P, _T]: ...
 
     extra: dict[str, Any]
     depth: int
     reraise: bool
-
 
 class Logger:
     base: Any
@@ -34,7 +28,6 @@ class Logger:
     @staticmethod
     @overload
     def io(func: Callable[_P, _T]) -> Callable[_P, _T]: ...
-
     @staticmethod
     @overload
     def io(
